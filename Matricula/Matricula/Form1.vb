ee868b9d1d3c2bd976ed6dac6525721561bd0002
Lucia Santos <analucia0189@gmail.com>
2015-05-05 07:54:38 -0600

    mass

--- Matricula/Matricula/Form1.vb
+++ Matricula/Matricula/Form1.vb
@@ -4,8 +4,7 @@
     Private clases, labs As String()
     Private totalCuenta, mensualidadPago As Double
     Private cantidadMensualidades As Integer
-    Private precioClase, precioLab As Double
-
+    Private precioClase, precioLaboratorio, precio As Double
 
     Private Sub nombrePersona_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles nombrePersona.KeyPress
         e.Handled = Not (Char.IsLetter(e.KeyChar) Or Asc(e.KeyChar) = Keys.Back Or Asc(e.KeyChar) = Keys.Space)
@@ -13,5 +12,23 @@
 
     Private Sub nombreClaseLab_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles nombreClaseLab.KeyPress
         e.Handled = Not (Char.IsLetterOrDigit(e.KeyChar) Or Asc(e.KeyChar) = Keys.Back Or Asc(e.KeyChar) = Keys.Space)
+    End Sub
+
+    Private Sub tipoClaseLab_SelectedIndexChanged(sender As Object, e As EventArgs) Handles tipoClaseLab.SelectedIndexChanged
+        Select tipoClaseLab.SelectedItem
+            Case "Clase"
+                precio = 150
+                precioClaseLab.Text = "150"
+            Case "Laboratorio"
+                precio = 100
+                precioClaseLab.Text = "100"
+            Case Else
+                precio = 0
+                precioClaseLab.Text = ""
+        End Select
+    End Sub
+
+    Private Sub agregarClase_Click(sender As Object, e As EventArgs) Handles agregarClase.Click
+        Debug.WriteLine("Voy a añadir la clase")
     End Sub
 End Class
