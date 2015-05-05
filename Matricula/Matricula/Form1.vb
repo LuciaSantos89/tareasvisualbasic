64c08cc475c1b4a56c37281c7477153fe5c5a2f2
Lucia Santos <analucia0189@gmail.com>
2015-05-05 00:13:59 -0600

    mas

--- Matricula/Matricula/Form1.vb
+++ Matricula/Matricula/Form1.vb
@@ -1,5 +1,12 @@
 ﻿Public Class Matricula
 
+    Private nombrePer As String
+    Private clases, labs As String()
+    Private totalCuenta, mensualidadPago As Double
+    Private cantidadMensualidades As Integer
+    Private precioClase, precioLab As Double
+
+
     Private Sub nombrePersona_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles nombrePersona.KeyPress
         e.Handled = Not (Char.IsLetter(e.KeyChar) Or Asc(e.KeyChar) = Keys.Back Or Asc(e.KeyChar) = Keys.Space)
     End Sub
